aa27b7ae8ae32d4b4055f1fa26dccf0e7b1e0580
Ajay Gopi <ajaygopi98@berkeley.edu>
2017-10-07 01:48:19 -0700

    added the new create

--- ask_quizlet.py
+++ ask_quizlet.py
@@ -7,16 +7,57 @@ ask = Ask(app, "/")
 @ask.launch
 def start_skill():
     welcome_message = "Welcome to Ask Quizlet! Would you like to study or create a flash card set?"
+    session.attributes["test_num"] = 0
+    session.attributes["final_set"] = {};
+    session.attributes["currentkey"] = null
     return question(welcome_message)
 
+test_questions = ["ant", "bee", "cow", "dog"]
 @ask.intent("StudyIntent")
-def share_headlines():
-    msg = "Not implemented."
-    return statement(msg)
+def study():
+    msg = "Please say back the word: "
+    msg += test_questions[session.attributes["test_num"]]
+    return question(msg)
 
-@ask.intent("CreateIntent")
-def share_headlines():
-    msg = "Not implemented."
+@ask.intent("AnswerIntent")
+def answer(ans):
+    if ans == test_questions[session.attributes["test_num"]]:
+        session.attributes['test_num'] += 1
+        if session.attributes["test_num"] == len(test_questions):
+            return statement("Congrats, you said all the words.")
+        return study()
+    return question("You fucked up.")
+
+#Trying to create and word with a definition
+# @ask.intent("CreateIntent")
+# def create():
+#     msg = "Please tell the word: "
+#     return question(msg)
+#
+# #For adding the word
+# @ask.intent("NewWordIntent")
+# def newWord(actualWord):
+#     if (newWord not in session.attributes["final_set"].keys()):
+#         session.attributes["final_set"][actualWord] = null
+#         session.attributes["currentkey"] = actualWord
+#         msg = "Please say the definition"
+#         return question(msg)
+#     #return create()
+#
+#
+# #For adding the definition
+# @ask.intent("NewDefinitionIntent")
+# def add_definition(definition):
+#     session.attributes["final_set"][session.attributes["currentkey"]] = definition;
+#     msg = "Word and definition added." + session.attributes["currentkey"] + "means" + definition
+#     return question(msg)
+
+
+
+
+@ask.intent("AMAZON.StopIntent")
+def exit():
+    msg = "Quitting Ask Quizlet."
     return statement(msg)
 
 if __name__ == '__main__':
