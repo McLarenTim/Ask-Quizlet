9576afd10cd2cbf6f46e5032d293f42cc71ed0ad
Dennis <denniscfeng@gmail.com>
2017-10-07 14:13:43 -0700

    doing testing asking from a study set, doesnt work perfectly

--- ask_quizlet.py
+++ ask_quizlet.py
@@ -1,30 +1,28 @@
 from flask import Flask
 from flask_ask import Ask, statement, question, session
+from random import choice
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-state_map = {
-    "start": ["create", "study", "exit"],
-    "study": ["await_ans", "exit"],
-    "await_ans": ["study", "exit"]
-}
-
-test_questions = ["ant", "bob", "cow", "dog"]
-
 @ask.launch
 def start_skill():
     welcome_message = "Welcome to Ask Quizlet! Would you like to study or create a flash card set?"
-    session.attributes["test_num"] = 0
-    session.attributes["final_set"] = {}
+    session.attributes["set"] = {}
+    session.attributes["test_set"] = {
+        "ascertain": "discover through examination or experimentation; determine",
+        "avenge": "take revenge on or get satisfaction for; take vengeance on behalf of",
+        "chicanery": "deception by trickery; trick",
+        "remiss": "lax in attending to duty; exhibiting carelessness",
+        "ruminate": "meditate at length; ponder; muse; chew cud"
+    }
     session.attributes["prev"] = "anything"
-
-    #session.attributes["currentkey"] = null
+    session.attributes["current_correctness"] = 0
+    session.attributes["current_word"] = 0
     return question(welcome_message)
 
 @ask.intent("CreateIntent")
 def create():
-    print("create")
     if session.attributes["state"] not in ["start"]:
         return question("Invalid Input.")
     msg = "Not implemented."
@@ -32,28 +30,34 @@ def create():
 
 @ask.intent("StudyIntent")
 def study():
-    #only if have not called any other function
-    if (session.attributes["prev"] == "anything" or session.attributes["prev"] == "answer"):
-        msg = "Please say back the word: "
-        msg += test_questions[session.attributes["test_num"]]
-        session.attributes["prev"] = "study"
+    if (session.attributes["prev"] == "anything"):
+        # session.attributes["current_word"] = choice(list(session.attributes["set"].keys()))
+        msg = ""
+        if session.attributes["current_correctness"] != 0:
+            if session.attributes["current_correctness"]:
+                msg += "Correct! "
+            else:
+                msg += "Incorrect. The word was: " + session.attributes["current_word"] + ". "
+        session.attributes["current_word"] = choice(list(session.attributes["test_set"].keys()))
+        msg += "What is the word for: " + session.attributes["test_set"][session.attributes["current_word"]]
+        session.attributes["prev"] = "answer"
         return question(msg)
     else:
-        msg = "Please continue the function or stop"
+        msg = "Please continue the function or stop."
         return question(msg)
 
 #prev word = answer
 @ask.intent("AnswerIntent")
 def answer(ans):
     if (session.attributes["prev"] == "answer"):
-        if ans == test_questions[session.attributes["test_num"]]:
-            session.attributes['test_num'] += 1
-            if session.attributes["test_num"] == len(test_questions):
-                return statement("Congrats, you said all the words.")
-            return study()
-        return question("You fucked up.")
+        if ans == session.attributes["current_word"]:
+            session.attributes["current_correctness"] = True
+        else:
+            session.attributes["current_correctness"] = False
+        session.attributes["prev"] = "anything"
+        return study()
     else:
-        msg = "Please continue the function or stop"
+        msg = "Please continue the function or stop."
         return question(msg)
 
 #Trying to create and word with a definition
@@ -62,7 +66,7 @@ def answer(ans):
 def create():
     if(session.attributes["prev"] == "anything"):
         session.attributes["prev"] = "create"
-        msg = "Please tell the word: "
+        msg = "Please say the word."
         return question(msg)
     else:
         msg = "Please continue the proper function or stop"
@@ -73,14 +77,13 @@ def create():
 #prev word = newword
 @ask.intent("NewWordIntent")
 def newWord(realword):
-
     if(session.attributes["prev"] == "create"):
-        if (newWord not in session.attributes["final_set"].keys()):
-            #session.attributes["final_set"][actualWord] = null
+        if (newWord not in session.attributes["set"].keys()):
+            #session.attributes["set"][actualWord] = null
             #setting prev to newWord, so can only access add_definition after this
             session.attributes["prev"] = "newword"
             session.attributes["currentkey"] = realword
-            msg = "Please say the definition"
+            msg = "Please say the definition."
             return question(msg)
     else:
         msg = "Please continue the function or stop"
@@ -94,10 +97,10 @@ def newWord(realword):
 @ask.intent("NewDefinitionIntent")
 def add_definition(definition):
     if(session.attributes["prev"] == "newword"):
-        session.attributes["final_set"][session.attributes["currentkey"]] = definition
+        session.attributes["set"][session.attributes["currentkey"]] = definition
         #setting the prev so one can access anything
         session.attributes["prev"] = "anything"
-        msg = "Word and definition added " + session.attributes["currentkey"] + "means" + definition
+        msg = "Word and definition added: " + session.attributes["currentkey"] + " means: " + definition
         return question(msg)
     else:
         msg = "Please continue the function or stop"
@@ -109,6 +112,8 @@ def add_definition(definition):
 
 @ask.intent("AMAZON.StopIntent")
 def exit():
+    # if (session.attributes["prev"] == "newword"):
+    #     return start_skill()
     msg = "Quitting Ask Quizlet."
     return statement(msg)
 
